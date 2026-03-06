d7ed4c49e361dd0e3efaa6c3c84578dc155b4d28
Daviansyah07 <daviansyahdavi5@gmail.com>
2026-03-06 14:47:44 +0700

    menambahkan table

--- database/migrations/2026_03_06_074453_create_referensi_table.php
+++ database/migrations/2026_03_06_074453_create_referensi_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('references', function (Blueprint $table) {
+        Schema::create('referensi', function (Blueprint $table) {
             $table->id();
             $table->string('nama_persyaratan',200);
             $table->text('keterangan');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('references');
+        Schema::dropIfExists('referensi');
     }
 };
